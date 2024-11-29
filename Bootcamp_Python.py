d91bcead5b320e46f6b82f32da38ddb4d510298e
gbentosilva <gabrielbento9812@gmail.com>
2024-11-29 14:46:11 -0300

    moved files to new foldr

---
+++ Bootcamp_Python.py
@@ -0,0 +1,6 @@
+nome = input("digite seu nome: ")
+salario = int(input("digite seu salário: "))
+bonus = int(input("digite seu bônusga: "))
+
+valor_do_bonus = salario + bonus
+print(f"o usuário {nome} possui o bônus de {valor_do_bonus}")
